0d6b144b2e7815e38403c9f4e8451f6c9d88bb8e
Adnan <md.ashikadnan@gmail.com>
2019-07-26 14:05:31 +1000

    Currently working on it.

--- superhero-app/src/App.js
+++ superhero-app/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      superheroes: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://api.myjson.com/bins/zuh4h")
+      .then(response => response.json())
+      .then(data => this.setState({ superheroes: data }));
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.superheroes.map(hero => (
+          <h1 key={hero.id}>{hero.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;

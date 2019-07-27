6f4ee51fff7986f7e52bc53be14132139a5b8979
Adnan <md.ashikadnan@gmail.com>
2019-07-28 02:03:43 +1000

    Added search-box component

--- superhero-app/src/App.js
+++ superhero-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      superheroes: []
+      superheroes: [],
+      searchField: ""
     };
   }
   componentDidMount() {
@@ -14,10 +16,21 @@ class App extends Component {
       .then(response => response.json())
       .then(data => this.setState({ superheroes: data }));
   }
+  // If we want to see or do something with our state right after we set it then
+  // we have to do it inside the second argument in the setState function. Second callback
+  //function will be called right after the setState function.
   render() {
+    const { superheroes, searchField } = this.state;
+    const filterHeories = superheroes.filter(hero =>
+      hero.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList superheroes={this.state.superheroes} />
+        <SearchBox
+          placeholder="Search Name"
+          handlechange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList superheroes={filterHeories} />
       </div>
     );
   }
